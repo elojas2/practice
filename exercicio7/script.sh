5f76e4960acc51aa34c6f062611ed9c7a2cfbaf8
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-01 11:03:52 -0300

    apanhando pro if por esquecer aspas duplas

--- exercicio7/script.sh
+++ exercicio7/script.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 echo $HOME"/Downloads"
 
-DIRETORIO=$HOME"/Downloads"
+DIRETORIO=$HOME"/Downloads/"
 DIRETORIO_DESTINO="/tmp"
 
-#echo $DIRETORIO
-cd "$DIRETORIO"
-
-ultimos_arquivos=$(ls -t | tail -10)
-for i in $teste 
+opcao="m"
+ultimos_arquivos=$(ls -t $DIRETORIO | tail -10)
+for i in $ultimos_arquivos 
 do 
-    echo $i
+ #  echo $(ls -lah $DIRETORIO$i)
+    if [ "$opcao" = "m" ]
+    then
+        echo "m"
+        echo $(cp -v $DIRETORIO$i $DIRETORIO_DESTINO)
+    elif [ "$opcao" = "a" ]
+    then
+        echo "a"
+        echo $(cp -v $DIRETORIO$i $DIRETORIO_DESTINO)
+    else 
+        echo "escolha a opcao certa"
+    fi
+
 done
 
 
-NOME_FILE="lista.txt"
 
-echo "$ultimos_arquivos'n" >> $NOME_FILE
+
 
 #cd "$DIRETORIO"
+#echo $DIRETORIO
 #DIRETORIO="../Documents/github/practice/exercicio7/"
 #echo $teste >> /Users/eloyse.fernanda/Documents/github/practice/exercicio7/anotacoes.txt
