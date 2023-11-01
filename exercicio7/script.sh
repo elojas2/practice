b640753d6ee291a46c995a112260867c6feee575
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-01 11:59:38 -0300

    finalizando

--- exercicio7/script.sh
+++ exercicio7/script.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
-echo $HOME"/Downloads"
+#echo $HOME"/Downloads"
 
 DIRETORIO=$HOME"/Downloads/"
 DIRETORIO_DESTINO="/tmp"
 
-opcao="m"
 ultimos_arquivos=$(ls -t $DIRETORIO | tail -10)
 for i in $ultimos_arquivos 
 do 
- #  echo $(ls -lah $DIRETORIO$i)
-    if [ "$opcao" = "m" ]
+#echo $(ls -lah $DIRETORIO$i)
+    echo "para o arquivo: "$i
+
+    echo "Digite a opcao m para mover e a para apagar:"
+    read opcao;
+    
+    if [ "$opcao" = "a" ]
     then
-        echo "m"
-        echo $(cp -v $DIRETORIO$i $DIRETORIO_DESTINO)
-    elif [ "$opcao" = "a" ]
+        echo $(rm -v $DIRETORIO$i)
+    elif [ "$opcao" = "m" ]
     then
-        echo "a"
-        echo $(cp -v $DIRETORIO$i $DIRETORIO_DESTINO)
+        echo "Digite o destino a partir da home:"
+        read destino;
+        echo $(mv -v "$DIRETORIO$i" "$HOME/$destino/")
     else 
         echo "escolha a opcao certa"
     fi
-
 done
-
-
-
-
-
-#cd "$DIRETORIO"
-#echo $DIRETORIO
-#DIRETORIO="../Documents/github/practice/exercicio7/"
-#echo $teste >> /Users/eloyse.fernanda/Documents/github/practice/exercicio7/anotacoes.txt
