4ddce0705609e85409a4b18314d459aa6c5fad6a
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-01 08:03:38 -0300

    ajuda do travi

--- exercicio7/script.sh
+++ exercicio7/script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-DIRETORIO="../../../../Downloads/"
+echo $HOME"/Downloads"
+
+DIRETORIO=$HOME"/Downloads"
+DIRETORIO_DESTINO="/tmp"
+
+#echo $DIRETORIO
 cd "$DIRETORIO"
 
-teste=$(ls -t | tail -10)
+ultimos_arquivos=$(ls -t | tail -10)
+for i in $teste 
+do 
+    echo $i
+done
+
 
 NOME_FILE="lista.txt"
-DIRETORIO="../Documents/github/practice/exercicio7/"
 
-cd "$DIRETORIO"
-echo "$teste'n" >> $NOME_FILE
+echo "$ultimos_arquivos'n" >> $NOME_FILE
 
-
+#cd "$DIRETORIO"
+#DIRETORIO="../Documents/github/practice/exercicio7/"
 #echo $teste >> /Users/eloyse.fernanda/Documents/github/practice/exercicio7/anotacoes.txt
