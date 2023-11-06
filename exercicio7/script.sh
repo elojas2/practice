d80c1005cb7d78e415b760c1c27119defb52eaef
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-06 11:30:59 -0300

    adicao do while

--- exercicio7/script.sh
+++ exercicio7/script.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
-#echo $HOME"/Downloads"
-
-DIRETORIO=$HOME"/Downloads/"
+DIRETORIO="$HOME/Downloads/"
 DIRETORIO_DESTINO="/tmp"
 
-ultimos_arquivos=$(ls -t $DIRETORIO | tail -10)
-for i in $ultimos_arquivos 
-do 
-#echo $(ls -lah $DIRETORIO$i)
-    echo "para o arquivo: "$i
+ultimos_arquivos=($(ls -t "$DIRETORIO" | tail -10))
 
-    echo "Digite a opcao m para mover e a para apagar:"
-    read opcao;
-    
-    if [ "$opcao" = "a" ]
+while true
+do
+    echo "Digite a opção 'm' para mover e 'a' para apagar, ou 'q' para sair:"
+    read opcao
+
+    if [ "$opcao" = "q" ]
     then
-        echo $(rm -v $DIRETORIO$i)
-    elif [ "$opcao" = "m" ]
-    then
-        echo "Digite o destino a partir da home:"
-        read destino;
-        echo $(mv -v "$DIRETORIO$i" "$HOME/$destino/")
-    else 
-        echo "escolha a opcao certa"
+        break
     fi
+
+    while [[ $opcao != "m" && $opcao != "a" ]]
+    do
+        echo "Opção inválida, escolha 'a' para apagar ou 'm' para mover:"
+        read opcao
+    done
+
+    for arquivo in "${ultimos_arquivos[@]}"
+    do
+        echo "Para o arquivo: $arquivo"
+        
+        if [ "$opcao" = "a" ]
+        then
+            echo "Você tem certeza que deseja apagar o arquivo $arquivo? (s/n)"
+            read resposta
+            if [ "$resposta" = "s" ]
+            then
+                echo "Arquivo apagado!" $(rm -v "$DIRETORIO$arquivo")
+            elif [ "$resposta" = "n" ]
+            then
+                echo "Arquivo não será apagado"
+            else
+                echo "Opção inválida"
+            fi
+        elif [ "$opcao" = "m" ]
+        then
+            echo "Digite o destino a partir da home:"
+            read destino
+            echo $(mv -v "$DIRETORIO$arquivo" "$HOME/$destino/")
+        fi
+        echo "deseja escolher outra opcao? (s/n)"
+        read resposta
+        if [ "$resposta" = "s" ]; then
+            break
+        fi
+    done
 done
