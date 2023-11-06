2fcb411b917476023c164fcd882a8b6cf633041b
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-06 15:56:38 -0300

    ajuste no while

--- exercicio7/script.sh
+++ exercicio7/script.sh
@@ -4,49 +4,46 @@ DIRETORIO_DESTINO="/tmp"
 
 ultimos_arquivos=($(ls -t "$DIRETORIO" | tail -10))
 
-while true
-do
-    echo "Digite a opção 'm' para mover e 'a' para apagar, ou 'q' para sair:"
-    read opcao
+# while true
+# do
+#     read opcao
 
-    if [ "$opcao" = "q" ]
-    then
-        break
-    fi
+#     if [ "$opcao" = "q" ]
+#     then
+#         break
+#     fi
 
-    while [[ $opcao != "m" && $opcao != "a" ]]
-    do
-        echo "Opção inválida, escolha 'a' para apagar ou 'm' para mover:"
-        read opcao
-    done
-
+#     while [[ $opcao != "m" && $opcao != "a" ]]
+#     do
+#         echo "Opção inválida, escolha 'a' para apagar ou 'm' para mover:"
+#         read opcao
+#     done
+controller="1"
     for arquivo in "${ultimos_arquivos[@]}"
     do
-        echo "Para o arquivo: $arquivo"
-        
-        if [ "$opcao" = "a" ]
-        then
+        while [ $controller = "1" ]
+        do
+            echo "Para o arquivo: $arquivo"
+            echo "Digite a opção 'm' para mover e 'a' para apagar, ou 'q' para sair:"
+            read opcao
             echo "Você tem certeza que deseja apagar o arquivo $arquivo? (s/n)"
             read resposta
+            
             if [ "$resposta" = "s" ]
             then
-                echo "Arquivo apagado!" $(rm -v "$DIRETORIO$arquivo")
-            elif [ "$resposta" = "n" ]
-            then
-                echo "Arquivo não será apagado"
-            else
-                echo "Opção inválida"
+                if [ "$opcao" = "a" ]
+                then
+                    echo "Arquivo apagado!" $(rm -v "$DIRETORIO$arquivo")
+                elif [ "$opcao" = "m" ]
+                then
+                    echo "Digite o destino a partir da home:"
+                    read destino
+                    echo $(mv -v "$DIRETORIO$arquivo" "$HOME/$destino/")
+                fi
             fi
-        elif [ "$opcao" = "m" ]
-        then
-            echo "Digite o destino a partir da home:"
-            read destino
-            echo $(mv -v "$DIRETORIO$arquivo" "$HOME/$destino/")
-        fi
-        echo "deseja escolher outra opcao? (s/n)"
-        read resposta
-        if [ "$resposta" = "s" ]; then
-            break
-        fi
+            if [[ $opcao != "m" && $opcao != "a" ]]
+            then
+                controller="0"
+            fi
+        done 
     done
-done
